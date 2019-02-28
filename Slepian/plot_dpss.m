dba5afb8e56c9d73d8a6f9afdef7a1ab39eaa681
Ke Chen <ke7@ualberta.ca>
2019-02-28 13:41:14 -0800

    add a folder

--- Slepian/plot_dpss.m
+++ Slepian/plot_dpss.m
@@ -7,3 +7,10 @@ A = fread(fileID,[64,4],'double');
 plot(A);
 xlabel('n');
 title('Discrete Prolate Spheroidal Sequences');
+%hold on;
+seq = dpss(64,4,4);
+%plot(seq);
+figure;
+plot(seq-A);
+title('Difference');
+
