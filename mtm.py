58e18a91f44fe28de53e13617d4a4e856c49f869
Ke Chen <ke7@ualberta.ca>
2019-03-09 11:47:20 -0800

    modify code

--- mtm.py
+++ mtm.py
@@ -1,6 +1,16 @@
 def ricker(f, dt):
     """
-    Ricker wavelet
+    Ricker wavelet of central frequency f
+    
+    IN    f : central frequency in Hz
+          dt : time sampling interval in sec
+          
+    OUT   the Ricker wavelet
+    
+    Example
+        wavelet = ricker(10, 0.002)
+        
+    Credit: Ke Chen, kechen@lbl.gov
     """
     
     tw = 2 * 1.35 * math.sqrt(6.0)/math.pi/f
@@ -9,7 +19,7 @@ def ricker(f, dt):
 
     nc = math.floor(nw/2)
     
-    k = np.arange(1,nw)
+    k = np.arange(1,nw+1)
     
     alpha = (k - nc - 1)*dt*f*math.pi
     
@@ -25,16 +35,17 @@ def shift_signal(d, delay):
     """
     nextpow2 = math.ceil(math.log2(nt))
     nfft = int(math.pow(2, nextpow2))
+
     D = np.fft.fft(d,nfft)
     S = np.zeros(nfft, dtype=complex)
 
-    for i in range(0, round(nfft/2)):
+    for i in range(0, round(nfft/2) + 1):
         w = 2.0*math.pi*i/nfft/dt
         Shift = cmath.exp(-1j*w*delay)
         S[i] = D[i]*Shift
 
     # frequency domain symmetries
-    for i in range(1,round(nfft/2)-1):
+    for i in range(1,round(nfft/2)):
         S[nfft - i] = np.conjugate(S[i])
 
     s = np.fft.ifft(S)
@@ -48,6 +59,7 @@ import math
 import cmath
 import matplotlib.pyplot as plt
 
+# 1 - Read/synthetize data
 tmax = 12
 dt = 0.002
 nt = math.floor(tmax/dt) + 1
@@ -60,7 +72,8 @@ d = np.zeros(nt)
 d[round(nt/2)] = 1.0
 d = np.convolve(d,wavelet,'same')
 
-s= shift_signal(d, 1.0)
+tdelay = 0.0
+s= shift_signal(d, tdelay)
 
 plt.plot(t,d,'r')
 plt.plot(t,s,'b')
@@ -69,10 +82,14 @@ plt.ylabel('Amplitude')
 plt.title('Observed and synthetic data')
 plt.show()
 
-#window signal 4-9s
-d = d[2000:4500]
-s = s[2000:4500]
-t = np.arange(2000*dt,4500*dt,dt)
+# 2 - Butterwoth bandpass filter data (optional)
+
+# 3 - Windowing
+n1 = 2800
+n2 = 3200
+d = d[n1:n2]
+s = s[n1:n2]
+t = np.arange(n1*dt,n2*dt,dt)
 nt = len(t)
 plt.plot(t,d,'r')
 plt.plot(t,s,'b')
@@ -81,10 +98,28 @@ plt.ylabel('Amplitude')
 plt.title('Windowed observed and synthetic data')
 plt.show()
 
-#DPSS
+# 4 - compute_cc
+
+# 5 - deconstruct_dat_cc (Apply CC -\delta T and -\delta A to the observed data prior to MTM)
+
+# 6 - compute_average_error (sigma_t)
+
+# 7 - FFT parameters
+nextpow2 = math.ceil(math.log2(nt))
+nfft = int(math.pow(2, nextpow2))
+f0 = 0.0
+df = 1.0/(nfft*dt)
+dw = 2*math.pi*df
+fnum = int(nfft/2) + 1
+w = np.zeros(nfft)
+w[0:fnum] = dw * np.arange(0,fnum)
+w[fnum:nfft] = dw * np.arange(-fnum + 2, 0)
+
+# 8 - DPSS
 M = len(d)
 NW = 2.5
 Kmax = 5
+ntaper = Kmax
 dpss = signal.windows.dpss(M, NW, Kmax)
 for i in range(0, Kmax):
     plt.plot(t,dpss[i,:])
@@ -93,26 +128,17 @@ plt.ylabel('Amplitude')
 plt.title('First five 2.5pi DPSS')
 plt.show()
 
-#fft parameters
-f0 = 0.0
-nextpow2 = math.ceil(math.log2(nt))
-nfft = int(math.pow(2, nextpow2))
-df = 1.0/(nfft*dt)
-dw = 2*math.pi*df
-fnum = int(nfft/2) + 1
-ntaper = Kmax
-
-#Transfer function
-wtr = 1.0e-10
+# 9 - Transfer function
+wtr = 1.0e-10     #water level
 T = np.zeros(fnum, dtype=complex)
 A = np.zeros(fnum, dtype=complex)
 B = np.zeros(fnum, dtype=complex)
 
 for i in range(0, ntaper-1):
-    #apply taper
+    #apply time domain taper
     dtp = d*dpss[i,:]
     stp = s*dpss[i,:]
-    #FFT
+    #apply FFT
     DTP = np.fft.fft(dtp,nfft)
     STP = np.fft.fft(stp,nfft)
     #
@@ -126,14 +152,14 @@ for i in range(0, ntaper-1):
     plt.title('DPSS tapered observed data')
 plt.show()
 
-for k in range(0,fnum):
+for k in range(0, fnum):
     T[k] = A[k] / B[k]
 
 #calculate phase
 tau = np.zeros(fnum)
 
 for k in range(0,fnum):
-    tau[k] =math.atan2(T[k].imag, T[k].real)
+    tau[k] = math.atan2(T[k].imag, T[k].real)
 
 faxis = np.arange(0,fnum)*df
 plt.plot(faxis,tau)
@@ -141,4 +167,5 @@ plt.xlabel('Frequency')
 plt.ylabel('Time delay')
 plt.title('Frequency-dependent time delay')
 plt.show()
+
 
